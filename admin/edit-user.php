415a45ce0ca1e3c642bac4253b1d90e5e42767cf
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-21 23:38:40 +0200

    started working on edit user (#11)

--- admin/edit-user.php
+++ admin/edit-user.php
@@ -1,5 +1,17 @@
 <?php
 include 'partials/header.php';
+
+if(isset($_GET['id'])) {
+	$id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+	$query = "SELECT * FROM users WHERE id=$id";
+	$result = mysqli_query($connection, $query);
+	$user = mysqli_fetch_assoc($result);
+} else {
+	header('location: ' . ROOT_URL . 'admin/manage-users.php');
+	die();
+}
+
+
 ?>
 
 
@@ -7,18 +19,14 @@ include 'partials/header.php';
 <section class="form__section">
 <div class="container form__section-container">
     <h2>Edit User</h2>
-    <form action="" enctype="multipart/form-data">
-        <input type="text" placeholder="First Name">
-        <input type="text" placeholder="Last Name">
-        <select>
+    <form action="<?= ROOT_URL ?>admin/add-user-logic.php" method="POST">
+        <input type="text" value="<?= $user['firstname']?>" name="firstname" placeholder="First Name">
+        <input type="text" value="<?= $user['lastname']?>" name="lastname" placeholder="Last Name">
+        <select name="userrole">
             <option value="0">Author</option>
             <option value="1">Admin</option>
         </select>
-        <div class="form__control">
-            <label for="avatar"></label>
-            <input type="file" id="avatar">
-        </div>
-        <button type="submit" class="btn">Update User</button>
+        <button type="submit" name="submit" class="btn">Update User</button>
     </form>
 </div>
 </section>
