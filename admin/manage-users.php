0050635ccdb39310f791966943c4616e1a9477d0
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-27 23:15:45 +0200

    handled error for no users found

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -92,6 +92,7 @@ $users = mysqli_query($connection, $query);
         </aside>
         <main>
             <h2>Manage Users</h2>
+			<?php if(mysqli_num_rows($users) > 0) : ?>
             <table>
                 <thead>
                     <tr>
@@ -115,6 +116,11 @@ $users = mysqli_query($connection, $query);
                 
                 </tbody>
             </table>
+			<?php else : ?>
+				<div class="alert__message error">
+					<?= "No users found " ?>
+				</div>
+				<?php endif ?>
         </main>
       </div>
     </section>
