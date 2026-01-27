ab99bf282d17355f0a07073cffbe8fc73dc85901
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-27 22:36:39 +0200

    adding into the table and catching errors (#12)

--- admin/add-category-logic.php
+++ admin/add-category-logic.php
@@ -13,9 +13,20 @@ if(isset($_POST['submit'])) {
 		$_SESSION['add-category'] = "Enter the category's description bro bro!";		
 	}
 	
-	//redirect back to add category
+	//redirect back to add category page with form data if problems happen
 	if(isset($_SESSION['add-category'])){
-		
+		$_SESSION['add-category-data'] = $_POST;
+		header('location: ' . ROOT_URL . 'admin/add-category.php');
+		die();
+	}else{
+		//insert category into database
+		$query = "INSERT INTO categories (title, description) VALUES ('$title, $description')";
+		$result = mysqli_query($connection, $query);
+		if(mysqli_errno($connection)) {
+			$_SESSION['add-category'] = "couldn't add category";
+			header('location: ' ROOT_URL . 'admin/add-category.php');
+			die();
+		}
 	}
 	
 }
