d07f35517a28925442b5832eee4f247a2737e5a0
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-27 22:47:33 +0200

    add category works (#12)

--- admin/add-category-logic.php
+++ admin/add-category-logic.php
@@ -20,11 +20,15 @@ if(isset($_POST['submit'])) {
 		die();
 	}else{
 		//insert category into database
-		$query = "INSERT INTO categories (title, description) VALUES ('$title, $description')";
+		$query = "INSERT INTO categories (title, description) VALUES ('$title', '$description')";
 		$result = mysqli_query($connection, $query);
 		if(mysqli_errno($connection)) {
 			$_SESSION['add-category'] = "couldn't add category";
-			header('location: ' ROOT_URL . 'admin/add-category.php');
+			header('location: ' . ROOT_URL . 'admin/add-category.php');
+			die();
+		} else{
+			$_SESSION['add-category-success'] = "category added successfully";
+			header('location: ' . ROOT_URL . 'admin/manage-categories.php');
 			die();
 		}
 	}
