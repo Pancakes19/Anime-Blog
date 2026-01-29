45f7b334a967cf3bbd9ce31b72696757b4e27990
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-29 17:55:04 +0200

    delete category logic completed (#12)

--- admin/delete_category.php
+++ admin/delete_category.php
@@ -4,5 +4,13 @@ require 'config/database.php';
 if(isset($_GET['id'])) {
 	$id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
 	
+	
+	//for later
+	//update category _id of post that belong to this category to id of uncategorised category
+	
+	
+	//delete category
+	$query = "DELETE FROM categories WHERE id=$id LIMIT 1";
+	$result = mysqli_query($connection, $query);
 }
 
