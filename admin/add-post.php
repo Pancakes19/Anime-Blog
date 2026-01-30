2e944f70372f77a9d13961c962d58df27abff0c8
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-30 19:03:20 +0200

    added names to inputs (#9)

--- admin/add-post.php
+++ admin/add-post.php
@@ -10,21 +10,21 @@ include 'partials/header.php';
     <div class="alert__message error">
         <p>Sign In failed. Try again Broskii</p>
     </div>
-    <form action="" enctype="multipart/form-data" method="POST">
-        <input type="text" placeholder="Title">
-        <select>
+    <form action="<?= ROOT_URL ?>admin/add-post-logic.php" enctype="multipart/form-data" method="POST">
+        <input type="text" name="title" placeholder="Title">
+        <select name="category">
             <option value="1">Anime</option>
         </select>
-        <textarea rows="10" placeholder="Body"></textarea>
+        <textarea rows="10" name="body" placeholder="Body"></textarea>
         <div class="form__control inline">
-            <input type="checkbox" id="is_featured" checked>
+            <input type="checkbox" name="is_featured" value="1" id="is_featured" checked>
             <label for="is_featured" >Featured</label>
         </div>
         <div class="form__control">
             <label for="thumbnail">Add thumbnail</label>
-            <input type="file" id="thumbnail">
+            <input type="file" name="thumbnail" id="thumbnail">
         </div>
-        <button type="submit" class="btn">Add post</button>
+        <button type="submit" name="submit" class="btn">Add post</button>
     </form>
 </div>
 </section>
