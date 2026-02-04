82dab1a4ab10d75adbf313c9959ac98137ee2b7e
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-04 20:25:12 +0200

    photos and 1 file

---
+++ admin/edit-post-logic.php
@@ -0,0 +1,18 @@
+<?php 
+require 'config/database.php';
+
+//make sure edit post button was clicked
+if (isset($_POST['submit'])) {
+	$id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+	$previous_thumbnail_name = filter_var($_POST['previous_thumbnail_name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+	$title = filter_var($_POST['title'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+	$body = filter_var($_POST['body'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+	$category_id = filter_var($_POST['category_id'], FILTER_SANITIZE_NUMBER_INT);
+	$is_featured = filter_var($_POST['is_featured'], FILTER_SANITIZE_NUMBER_INT);
+	$thumbnail = $_FILES['thumbnail'];
+	
+	//set is featured to 0 it it was unchecked
+	$is_featured = $is_featured == 1 ?: 0;
+	
+	// 
+}
