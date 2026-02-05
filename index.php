86289e4f1ad14cb66a60da5bfcd7fc46b4853308
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-06 00:32:13 +0200

    showing featured post

--- index.php
+++ index.php
@@ -1,29 +1,36 @@
 <?php
 include 'partials/header.php';
 
+//fetch featured post from database
+$featured_query = "SELECT *FROM posts WHERE is_featured=1";
+$featured_result = mysqli_query($connection, $featured_query);
+$featured = mysqli_fetch_assoc($featured_result);
 
 ?>
 
 
 
-
+	<?php if(mysqli_num_rows($featured_result) ==1) : ?>
       <!--=======================Begining of Featured post=================-->
 
       <section class="featured">
         <div class="container featured__container">
           <div class="post__thumbnail">
-            <img src="./images/akaza2.png" >
+            <img src="./images/<?= $featured['thumbnail'] ?>" >
           </div>
           <div class="post__info">
-            <a href="category-post.html" class="category__button">Anime</a>
-            <h2 class="post__title"><a href="post.html">The Fierce Demise of Akaza: A Warrior’s Farewell</a></h2>
+		  <?php
+			//fetch cats
+			$category_id = $featured['category_id'];
+			$category_query = "SELECT * FROM categories WHERE id=$category_id";
+			$category_result = mysqli_query($connection, $category_query);
+			$category = mysqli_fetch_assoc($category_result);
+		  
+		  ?>
+            <a href="<?= ROOT_URL ?>category-post.php?id=<?= $category['id'] ?>" class="category__button"><?= $category['title']?></a>
+            <h2 class="post__title"><a href="<?= ROOT_URL ?>post.php?id=<?= $featured['id'] ?>"><?= $featured['title'] ?></a></h2>
             <p class="post__body">
-              Akaza did not deserve to die because, beneath his monstrous exterior, he was a deeply tragic 
-              figure shaped by pain, loss, and a desperate longing for strength born out of love and 
-              helplessness. Before becoming a demon, Akaza was Hakuji—a young man who endured unbearable 
-              suffering, losing his sick father and later his beloved fiancée, Koyuki, along with the life 
-              he had built. His transformation into a demon was not born from malice but from grief and 
-              despair, a result of circumstances that robbed him of everything he cared for. 
+              <?= substr($featured['body'], 0, 300) ?>....
             </p>
             <div class="post__author">
               <div class="post__author-avatar">
@@ -37,6 +44,7 @@ include 'partials/header.php';
           </div>
         </div>
       </section>
+	  <?php endif ?>
           <!--=======================End of featured post=============================-->
 
   <section class="posts">
