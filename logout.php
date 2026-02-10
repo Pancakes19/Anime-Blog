c436c67789b0f4cb138c93de4f3bf2964b895d0a
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-10 19:28:35 +0200

    fixing error message bugs (#18)

--- logout.php
+++ logout.php
@@ -1,6 +1,18 @@
 <?php
+require 'config/bootstrap.php';
 require 'config/constants.php';
-// kill all sessions and redirect user to home page
+
+// Clear all session variables
+$_SESSION = array();
+
+// Delete the session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time()-3600, '/');
+}
+
+// Destroy the session
 session_destroy();
+
+// Redirect to home
 header('location: ' . ROOT_URL);
 die();
