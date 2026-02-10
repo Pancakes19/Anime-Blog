51a614b0f886e5e5be7499b24bf3e3f62f9f448c
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-10 19:31:16 +0200

    fixing error message bugs (#18)

--- signin-logic.php
+++ signin-logic.php
@@ -1,9 +1,9 @@
 <?php
+require 'config/bootstrap.php';
 require 'config/database.php';
 
-
-if(isset($_POST['submit'])) {           
-	// get form database
+if(isset($_POST['submit'])) {
+	// get form data
 	$username_email = filter_var($_POST['username_email'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 	$password = filter_var($_POST['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 	
@@ -13,24 +13,33 @@ if(isset($_POST['submit'])) {
 		$_SESSION['signin'] = "Enter your password broskii";
 	} else{
 		//get user from database
-		$fetch_user_query = "SELECT * FROM users WHERE username= '$username_email' OR email='$username_email' ";
-		$fetch_user_result = mysqli_query($connection, $fetch_user_query);
+		$fetch_user_query = "SELECT * FROM users WHERE username=? OR email=?";
+		$stmt = mysqli_prepare($connection, $fetch_user_query);
+		mysqli_stmt_bind_param($stmt, "ss", $username_email, $username_email);
+		mysqli_stmt_execute($stmt);
+		$fetch_user_result = mysqli_stmt_get_result($stmt);
 		
-		if(mysqli_num_rows($fetch_user_result) ==1) {
-			//convert the record to an associative array
+		if(mysqli_num_rows($fetch_user_result) == 1) {
+			// Convert the record to an associative array
 			$user_record = mysqli_fetch_assoc($fetch_user_result);
 			$db_password = $user_record['password'];
-			//compare form passwordwith database password
+			
+			// Compare form password with database password
 			if(password_verify($password, $db_password)) {
-				//set session for access control
+				// Regenerate session ID to prevent session fixation
+				session_regenerate_id(true);
+				
+				// Set session for access control
 				$_SESSION['user-id'] = $user_record['id'];
-				//set session if user is an admin
+				
+				// Set session if user is an admin
 				if($user_record['is_admin'] == 1) {
 					$_SESSION['user_is_admin'] = true;
 				}
 				
-				//log normal admin in
+				// Redirect to admin dashboard
 				header('location: ' . ROOT_URL . 'admin/');
+				die();
 			} else { 
 				$_SESSION['signin'] = "I think something is wrong bruh!";
 			}
@@ -39,7 +48,7 @@ if(isset($_POST['submit'])) {
 		}
 	}
 	
-	//if any problem happens rediret to the signin
+	// If any problem happens, redirect to the signin page
 	if(isset($_SESSION['signin'])) {
 		$_SESSION['signin-data'] = $_POST;
 		header('location: ' . ROOT_URL . 'signin.php');
