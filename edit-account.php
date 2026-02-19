a2a2df8b6ed037730503f075abb4d518686a64ec
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-19 23:19:11 +0200

    trying to fix image errors (#27)

--- edit-account.php
+++ edit-account.php
@@ -4,57 +4,13 @@ include 'partials/header.php'
 
 <section class="dashboard">
 	
-	<?php if(isset($_SESSION['add-post-success'])) : //shows if post a post was a success?>
-		 <div class="alert__message success container">
-			<p>
-				<?= $_SESSION['add-post-success']; 
-				unset($_SESSION['add-post-success']);
-				?>
-			</p>
-		</div>
-	<?php elseif (isset($_SESSION['edit-post-success'])) : //shows if edit post was a success?>
-		 <div class="alert__message success container">
-			<p>
-				<?= $_SESSION['edit-post-success']; 
-				unset($_SESSION['edit-post-success']);
-				?>
-			</p>
-		</div>
-	<?php elseif (isset($_SESSION['edit-post'])) : //shows if edit post was not success?>
-		 <div class="alert__message error container">
-			<p>
-				<?= $_SESSION['edit-post']; 
-				unset($_SESSION['edit-post']);
-				?>
-			</p>
-		</div>
-	<?php elseif (isset($_SESSION['delete-post-success'])) : //shows if delete post was success?>
-		 <div class="alert__message success container">
-			<p>
-				<?= $_SESSION['delete-post-success']; 
-				unset($_SESSION['delete-post-success']);
-				?>
-			</p>
-		</div>
-	<?php elseif (isset($_SESSION['delete-post'])) : //shows if delete post failed?>
-		 <div class="alert__message error container">
-			<p>
-				<?= $_SESSION['delete-post']; 
-				unset($_SESSION['delete-post']);
-				?>
-			</p>
-		</div>
-	<?php endif ?>
 	
-      <div class="containder dashboard__container">
-        <button id="show__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-double-right"></i></button>
-        <button id="hide__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-double-left"></i></button>
         <aside>
-          
+          <img src="images/image.png" alt="">
         </aside>
         <main>
-            <section class="form__section">
-<div class="container form__section-container">
+          <section class="form__section">
+          <div class="container form__section-container">
     <h2>Edit User</h2>
     <form action="<?= ROOT_URL ?>admin/edit-user-logic.php" method="POST">
         <input type="hidden" value="<?= $user['id'] ?>" name="id" >
@@ -69,8 +25,8 @@ include 'partials/header.php'
         </select>
         <button type="submit" name="submit" class="btn">Update User</button>
     </form>
-</div>
-</section>
+    
+        </section>
         </main>
       </div>
     </section>
