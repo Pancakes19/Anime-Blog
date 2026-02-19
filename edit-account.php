26eee425dc50b0e5489b0c9043dc60b3f8bfab55
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-19 22:57:17 +0200

    added etid self user fields (#27)

--- edit-account.php
+++ edit-account.php
@@ -3,9 +3,29 @@ include 'partials/header.php'
 ?>
 
 
-    <div class="container dashboard__container">
-      
-    </div>
+
+
+    <section class="form__section">
+    <div class="container form__section-container">
+    <h2>Edit User</h2>
+    <form action="<?= ROOT_URL ?>admin/edit-user-logic.php" method="POST">
+        <input type="hidden" value="<?= $user['id'] ?>" name="id" >
+        <input type="text" value="<?= $user['firstname'] ?>" name="firstname" placeholder="First Name">
+        <input type="text" value="<?= $user['lastname'] ?>" name="lastname" placeholder="Last Name">
+        <input type="text" value="<?= $user['username'] ?>" name="username" placeholder="Username">
+        <input type="email" value="<?= $user['email'] ?>" name="email" placeholder="Email">
+
+        <select name="userrole">
+            <option value="0">Author</option>
+            <option value="1">Admin</option>
+        </select>
+        <button type="submit" name="submit" class="btn">Update User</button>
+    </form>
+</div>
+</section>
+
+
+
 
 
 <?php
