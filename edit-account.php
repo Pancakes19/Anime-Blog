5e15a28d66e17eee5b14584963b241da6fba5284
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-24 00:02:35 +0200

    link to edit account logic

--- edit-account.php
+++ edit-account.php
@@ -22,7 +22,7 @@ if(isset($_GET['id'])) {
           <section class="form__section">
           <div class="container form__section-container">
     <h2>Edit my details</h2>
-    <form action="<?= ROOT_URL ?>admin/edit-user-logic.php" method="POST">
+    <form action="<?= ROOT_URL ?>admin/edit-logic.php" method="POST">
         <input type="hidden" value="<?= $user['id'] ?>" name="id" >
         <input type="text" value="<?= $user['firstname'] ?>" name="firstname" placeholder="First Name">
         <input type="text" value="<?= $user['lastname'] ?>" name="lastname" placeholder="Last Name">
