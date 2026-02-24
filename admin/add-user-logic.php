e45581e1b5ddef70fb5987c0878fef03f23e1c75
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-24 21:09:21 +0200

    user creation logic security update | stmt

--- admin/add-user-logic.php
+++ admin/add-user-logic.php
@@ -3,95 +3,118 @@ require '../config/bootstrap.php';
 require '../config/session-timout.php';
 require 'config/database.php';
 
-//created table users
-//get form to sent to database when add user is clicked
 if(isset($_POST['submit'])) {
-	$firstname = filter_var($_POST['firstname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-	$lastname = filter_var($_POST['lastname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-	$username = filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-	$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-	$createpassword= filter_var($_POST['createpassword'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-	$confirmpassword = filter_var($_POST['confirmpassword'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-	$is_admin = filter_var($_POST['userrole'], FILTER_SANITIZE_NUMBER_INT);
-	$avatar = $_FILES['avatar'];
 
-	
-	//validate input 
-	if(!$firstname) {
-		$_SESSION['add-user'] = "Please enter your first name bro!";
-	} elseif(!$lastname) {
-		$_SESSION['add-user'] = "Please enter your last name bro!";
-	}elseif(!$username) {
-		$_SESSION['add-user'] = "Please enter your username bro!";
-	}elseif(!$email) {
-		$_SESSION['add-user'] = "Please enter a valid email bro!";
-	}elseif(strlen($createpassword) < 8 || strlen($confirmpassword) < 8) {
-		$_SESSION['add-user'] = "Password is too short bro!";
-	}elseif(!$avatar['name']) {
-		$_SESSION['add-user'] = "U need to add a Pfp bro!";
-	}else {
-		//check if passwords are matching
-		if($createpassword !== $confirmpassword) {
-			$_SESSION['add-user'] = "Your passwords don't match bruh! ";
-		} else{
-			//if they match then hash them
-			$hashed_password = password_hash($createpassword, PASSWORD_DEFAULT);
-			
-			//ok now we check if the username or email already exists
-			$user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' ";
-			$user_check_result = mysqli_query($connection, $user_check_query);
-			if (mysqli_num_rows($user_check_result) > 0) {
-				$_SESSION['add-user'] = "That Username or Email already exist bro!";
-			}else{
-				//cheking the avatar
-				//rename avatar with current timestamp
-				$time = time();
-				$avatar_name = $time . $avatar['name'];
-				$avatar_tmp_name = $avatar['tmp_name'];
-				$avatar_destination_path = '../images/' . $avatar_name;
-				
-				//make sure file is an image
-				$allowed_files = ['png', 'jpg', 'jpeg'];
-				$extention = explode('.', $avatar_name);
-				$extention = end($extention);
-				if(in_array($extention, $allowed_files)) {
-					//make sure the file is not too large (1mb+)
-					if($avatar['size'] < 1000000){
-						//upload avatar
-						move_uploaded_file($avatar_tmp_name, $avatar_destination_path);
-					}else {
-						$_SESSION['add-user'] = "Your image size is too big bro! Not more than 1mb";
-					}
-				}else {
-					$_SESSION['add-user'] = "We only allow .png, .jpg or jpeg file extensions bro!";
+    $firstname = trim($_POST['firstname']);
+    $lastname = trim($_POST['lastname']);
+    $username = trim($_POST['username']);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $createpassword = $_POST['createpassword'];
+    $confirmpassword = $_POST['confirmpassword'];
+    $is_admin = isset($_POST['userrole']) ? 1 : 0;
+    $avatar = $_FILES['avatar'];
 
-				}
-			}
-		}
-	}
+    // Validate input
+    if(!$firstname) {
+        $_SESSION['add-user'] = "Please enter your first name bro!";
+    } elseif(!$lastname) {
+        $_SESSION['add-user'] = "Please enter your last name bro!";
+    } elseif(!$username) {
+        $_SESSION['add-user'] = "Please enter your username bro!";
+    } elseif(!$email) {
+        $_SESSION['add-user'] = "Please enter a valid email bro!";
+    } elseif(strlen($createpassword) < 8) {
+        $_SESSION['add-user'] = "Password must be at least 8 characters bro!";
+    } elseif($createpassword !== $confirmpassword) {
+        $_SESSION['add-user'] = "Your passwords don't match bruh!";
+    } elseif(!$avatar['name']) {
+        $_SESSION['add-user'] = "You need to add a Pfp bro!";
+    }
 
-	//redirect to add user page if there are any problems
-	if(isset($_SESSION['add-user'])) {
-		//re-set the data back into the form
-		$_SESSION['add-user-data'] = $_POST;
-		header('location: ' . ROOT_URL . '/admin/add-user.php');
-		die();
-	} else {
-		//proceed to insert data into the database
-		$insert_user_query = "INSERT INTO users SET firstname='$firstname', lastname='$lastname', username='$username', email='$email', password='$hashed_password', avatar='$avatar_name', is_admin=$is_admin";
-		$insert_user_result = mysqli_query($connection, $insert_user_query);
-		
-		if(!mysqli_errno($connection)) {
-			//redirect to login with success message
-			$_SESSION['add-user-success'] = "$firstname $lastname just joined the family bro!";
-			header('location: ' . ROOT_URL . 'admin/manage-users.php');
-			die();
+    // Redirect back if validation fails
+    if(isset($_SESSION['add-user'])) {
+        $_SESSION['add-user-data'] = $_POST;
+        header('location: ' . ROOT_URL . '/admin/add-user.php');
+        exit();
+    }
 
-		}
-	}
-	
-} else{
-	//if the button was not cliked then go back to add-user page
-	header ('location: ' . ROOT_URL . 'admin/add-user.php');
-	die();
+    // Check if username or email already exists (SAFE)
+    $stmt = $connection->prepare(
+        "SELECT id FROM users WHERE username = ? OR email = ?"
+    );
+
+    $stmt->bind_param("ss", $username, $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($result->num_rows > 0) {
+        $stmt->close();
+        $_SESSION['add-user'] = "That Username or Email already exists bro!";
+        header('location: ' . ROOT_URL . '/admin/add-user.php');
+        exit();
+    }
+
+    $stmt->close();
+
+    // Handle avatar upload
+    $time = time();
+    $avatar_name = $time . '_' . basename($avatar['name']);
+    $avatar_tmp_name = $avatar['tmp_name'];
+    $avatar_destination_path = '../images/' . $avatar_name;
+
+    $allowed_files = ['png', 'jpg', 'jpeg'];
+    $extension = strtolower(pathinfo($avatar_name, PATHINFO_EXTENSION));
+
+    if(!in_array($extension, $allowed_files)) {
+        $_SESSION['add-user'] = "Only .png, .jpg or .jpeg allowed bro!";
+        header('location: ' . ROOT_URL . '/admin/add-user.php');
+        exit();
+    }
+
+    if($avatar['size'] > 1000000) {
+        $_SESSION['add-user'] = "Image must be less than 1MB bro!";
+        header('location: ' . ROOT_URL . '/admin/add-user.php');
+        exit();
+    }
+
+    // Hash password
+    $hashed_password = password_hash($createpassword, PASSWORD_DEFAULT);
+
+    // Insert user securely
+    $stmt = $connection->prepare(
+        "INSERT INTO users 
+        (firstname, lastname, username, email, password, avatar, is_admin) 
+        VALUES (?, ?, ?, ?, ?, ?, ?)"
+    );
+
+    $stmt->bind_param(
+        "ssssssi",
+        $firstname,
+        $lastname,
+        $username,
+        $email,
+        $hashed_password,
+        $avatar_name,
+        $is_admin
+    );
+
+    if($stmt->execute()) {
+
+        move_uploaded_file($avatar_tmp_name, $avatar_destination_path);
+
+        $_SESSION['add-user-success'] = "$firstname $lastname just joined the family bro!";
+        header('location: ' . ROOT_URL . 'admin/manage-users.php');
+        exit();
+
+    } else {
+        $_SESSION['add-user'] = "Something went wrong bro!";
+        header('location: ' . ROOT_URL . '/admin/add-user.php');
+        exit();
+    }
+
+    $stmt->close();
+
+} else {
+    header('location: ' . ROOT_URL . 'admin/add-user.php');
+    exit();
 }
